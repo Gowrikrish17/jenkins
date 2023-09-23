c4323f897fa929a5b2efb4f9b452fa5198afa4b8
Gowrikrish17 <143053027+Gowrikrish17@users.noreply.github.com>
2023-09-24 00:01:27 +0530

    Update Appmetric.java

--- Appmetric.java
+++ Appmetric.java
@@ -1,12 +1,14 @@
-public class AppTest
+public class Appmetric
 {
     public static void main( String[] args )
     {
     int a= 10, b=20, c=30;
     int d = a+b+c;
+        int e = d-a+c;
         System.out.println( "Value of var a "+a );
          System.out.println( "Value of var b "+b );
          System.out.println( "Value of var c "+c );
          System.out.println( "Value of var d "+d );
+          System.out.println( "Value of var e "+e );
     }
 }
