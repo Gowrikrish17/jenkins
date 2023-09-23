711a39792060869c357f0b90550fb2b092bcedcc
Gowrikrish17 <143053027+Gowrikrish17@users.noreply.github.com>
2023-09-23 23:26:20 +0530

    Update AppTest.java

--- AppTest.java
+++ AppTest.java
@@ -5,6 +5,9 @@ public class AppTest
     {
     int a= 10, b=20, c=30;
     int d = a+b+c;
-        System.out.println( "Welcome to branch1"+d );
+        System.out.println( "Value of var a "+a );
+         System.out.println( "Value of var b "+b );
+         System.out.println( "Value of var c "+c );
+         System.out.println( "Value of var d "+d );
     }
 }
